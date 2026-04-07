0fe7b3edc7c5ed393b3c7a09f59cf168af8e752a
TarasWRS <119341926+TarasWRS@users.noreply.github.com>
2026-04-07 21:42:28 +0300

    Завершив останнє завдання. Maximum frequency stack

---
+++ Maximum Frequency Stack/maximum_frequency_stack.py
@@ -0,0 +1,30 @@
+from collections import defaultdict, deque
+
+class FreqStack:
+
+    def __init__(self):
+        self.freq = defaultdict(int)
+        self.group = defaultdict(deque)
+        self.max_freq = 0
+
+    def push(self, val: int) -> None:
+        self.freq[val] += 1
+        f = self.freq[val]
+
+        if f > self.max_freq:
+            self.max_freq = f
+        self.group[f].append(val)
+
+    def pop(self) -> int:
+        value = self.group[self.max_freq].pop()
+        self.freq[value] -= 1
+
+        if not self.group[self.max_freq]:
+            self.max_freq -= 1
+        return value
+
+
+# Your FreqStack object will be instantiated and called as such:
+# obj = FreqStack()
+# obj.push(val)
+# param_2 = obj.pop()
